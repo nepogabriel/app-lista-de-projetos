8974ffc255bc7864d75b08f943c395bf7533c3fb
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2022-05-31 21:38:23 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lista_de_contatos',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
