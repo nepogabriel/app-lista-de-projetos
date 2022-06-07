72cecd8189dba76e19743edd0ce28eb4890f379e
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2022-06-07 00:06:22 -0300

    Routas funcionando

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -1,32 +1,35 @@
 <template>
   <layout-default>
-      <ion-list>
-        <ion-item router-link="/projeto/1">
-          <ion-label>Aplicativo Hamburgueria</ion-label>
-        </ion-item>
+    <ion-list>
+      <ion-item router-link="/projeto/1">
+        <ion-label>Aplicativo Hamburgueria</ion-label>
+      </ion-item>
 
-        <ion-item router-link="/projeto/2">
-          <ion-label>Sistema WEB Lava Jato</ion-label>
-        </ion-item>
+      <ion-item router-link="/projeto/2">
+        <ion-label>Sistema WEB Lava Jato</ion-label>
+      </ion-item>
 
-        <ion-item router-link="/projeto/3">
-          <ion-label>Aplicativo Clínica Médica</ion-label>
-        </ion-item>
+      <ion-item router-link="/projeto/3">
+        <ion-label>Aplicativo Clínica Médica</ion-label>
+      </ion-item>
 
-        <ion-item router-link="/projeto/4">
-          <ion-label>Sistema Reserva Hotelaria</ion-label>
-        </ion-item>
-      </ion-list>
+      <ion-item router-link="/projeto/4">
+        <ion-label>Sistema Reserva Hotelaria</ion-label>
+      </ion-item>
+    </ion-list>
+
+    <ion-button router-link="/registerProject">+</ion-button>
   </layout-default>
 </template>
 
 <script>
-  import {IonList, IonItem, IonLabel} from "@ionic/vue";
+  import {IonList, IonItem, IonLabel, IonButton} from "@ionic/vue";
   export default {
     components: {
       IonList,
       IonItem,
-      IonLabel
+      IonLabel,
+      IonButton,
     },
     methods:{
       navegaItemLista() {
@@ -35,3 +38,15 @@
     }
   }
 </script>
+
+<style scoped>
+ion-button {
+  position: fixed;
+  bottom: 20px;
+  right: 2.5rem;
+  height: 4.5rem;
+  width: 4.5rem;
+  font-size: 3rem;
+  --background: var(--ion-color-success-tint);
+}
+</style>
--- src/views/RegisterProjectPage.vue
+++ src/views/RegisterProjectPage.vue
@@ -3,38 +3,84 @@
     <ion-list>
       <ion-item>
         <ion-label position="stacked">Nome:</ion-label>
-        <ion-input></ion-input>
+        <ion-input v-model="nome"></ion-input>
       </ion-item>
 
       <ion-item>
         <ion-label position="stacked">Descrição</ion-label>
-        <ion-input></ion-input>
+        <ion-input v-model="descricao"></ion-input>
       </ion-item>
 
       <ion-item>
         <ion-label position="stacked">Valor</ion-label>
-        <ion-input></ion-input>
+        <ion-input v-model="valor"></ion-input>
       </ion-item>
 
       <ion-item>
         <ion-label position="stacked">Ações</ion-label>
-        <ion-textarea placeholder="Informe uma ação por linha"></ion-textarea>
+        <ion-textarea v-model="acoes" placeholder="Informe uma ação por linha"></ion-textarea>
       </ion-item>
 
       <ion-item>
         <ion-label position="stacked">Referências</ion-label>
-        <ion-textarea placeholder="Informe uma referência por linha"></ion-textarea>
+        <ion-textarea v-model="referencias" placeholder="Informe uma referência por linha"></ion-textarea>
       </ion-item>
     </ion-list>
+    <ion-button @click="saveLocal(nome, descricao, valor, acoes, referencias)" class="button button-save">Salvar</ion-button>
+
+    <ion-button class="button button-cancel" router-link="/home">x</ion-button>
   </layout-default>
 </template>
 
 <script>
-  import {IonInput} from '@ionic/vue';
+  import {IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton} from '@ionic/vue';
 
   export default {
     components: {
-      IonInput
+      IonList,
+      IonItem,
+      IonLabel,
+      IonInput,
+      IonTextarea,
+      IonButton,
+    },
+    data() {
+      return {
+        nome: '',
+        descricao: '',
+        valor: '',
+        acoes: '',
+        referencias: '',
+      }
+    },
+    methods: {
+      saveLocal(nome, descricao, valor, acoes, referencias) {
+        localStorage.setItem('projeto', JSON.stringify({
+          nome: nome,
+          descricao: descricao,
+          valor: valor,
+          acoes: acoes,
+          referencias: referencias
+        }))
+      }
     }
   }
 </script>
+
+<style scoped>
+.button {
+  position: fixed;
+  bottom: 20px;
+  min-width: 4.5rem;
+  height: 4.5rem;
+}
+.button-save {
+  left: 2.5rem;
+  font-size: 1.5rem;
+}
+  .button-cancel {
+    right: 2.5rem;
+    font-size: 3rem;
+    --background: var(--ion-color-danger);
+  }
+</style>
