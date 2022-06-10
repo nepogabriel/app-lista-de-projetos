3fcfe8e31a12eff3d39f29bda09bdfca955e35a1
Gabriel José Oliveira <78929152+gjoliveira@users.noreply.github.com>
2022-06-10 00:01:55 -0300

    página do projeto especifico

--- src/views/ProjectPage.vue
+++ src/views/ProjectPage.vue
@@ -1,16 +1,35 @@
 <template>
-  <ion-page>
+  <layout-default>
     <h1>Informações do Projeto</h1>
-
-  </ion-page>
-</template>
+    <ion-list>
+      <ion-item>
+        <ion-label>{{projeto.nome}}</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>{{projeto.descricao}}</ion-label>
+      </ion-item><ion-item>
+      <ion-label>{{projeto.acoes}}</ion-label>
+    </ion-item><ion-item>
+      <ion-label>{{projeto.valor}}</ion-label>
+    </ion-item>
+    </ion-list>
+    <ion-button class="button button-cancel" @click="voltar()" router-link="/home">
+      ←
+    </ion-button>
+  </layout-default>
+ </template>
 
 <script>
-  import {IonPage} from '@ionic/vue';
+  import { IonList, IonItem, IonLabel, IonButton} from '@ionic/vue';
+  import LayoutDefault from "@/views/LayoutDefault";
 
   export default {
     components: {
-      IonPage
+      LayoutDefault,
+      IonList,
+      IonItem,
+      IonLabel,
+      IonButton
     },
     data() {
       return {
@@ -18,10 +37,16 @@
         carregando: false,
       }
     },
+    mounted() {
+      this.getProjeto(this.$route.params.id);
+    },
     methods: {
-      async getProject() {
+      voltar() {
+        this.$router.go(-1);
+      },
+      async getProjeto(ID) {
         this.carregando = true;
-        return await fetch('http://apival.pousadatucanodomarahu.com.br/api/app/info/ID', {
+        return await fetch(`http://apival.pousadatucanodomarahu.com.br/api/app/info/${ID}`, {
           method: 'GET',
           headers: {
             Accept: 'application/json',
@@ -34,8 +59,9 @@
                 case response.status >= 100 && response.status < 200:
                   return false;
                 case response.status >= 200 && response.status < 300:
-                  this.projeto = response.json().then((json) => json);
-                  return true;
+                  return response.json().then((data) => {
+                    this.projeto = data;
+                  });
                 case response.status >= 400 && response.status < 500:
                   return false;
                 case response.status >= 500 && response.status < 600:
@@ -53,3 +79,14 @@
     }
   }
 </script>
+
+<style>
+.button {
+  position: fixed;
+  bottom: 20px;
+  min-width: 3.5rem;
+  height: 3.5rem;
+  right: 1.5rem;
+  font-size: 1.5rem;
+}
+</style>
